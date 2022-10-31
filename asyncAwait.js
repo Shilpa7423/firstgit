e6fbf423f8990728111852879b3825bd93bfe469
Shilpa7423 <shilpa7423@gmail.com>
2022-10-31 20:12:40 +0530

    asyncAwait

---
+++ asyncAwait.js
@@ -0,0 +1,57 @@
+console.log("person 1");
+console.log("person 2");
+
+async function movie()
+{
+    const getTicket=  new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            const a= 'true';
+            if(a=='true')
+                resolve('person 3 ticket bought');
+        else
+                reject('ticket sold out not there')
+        }
+        ,1000)
+    });
+
+const getPopcorn=  new Promise((resolve,reject)=>{
+        const a= 'true';
+        if(a=='true')
+            resolve('popcorn bought');
+    else
+            reject('no popcorn');
+});
+const getButter=  new Promise((resolve,reject)=>{
+    const a= 'true';
+    if(a=='true')
+        resolve('butter bought');
+else
+        reject('no butter');
+});
+const getColdDinks=  new Promise((resolve,reject)=>{
+    const a= 'true';
+    if(a=='true')
+        resolve('Cold drinks bought');
+else
+        reject('no cold drinks');
+});
+
+let ticket=await getTicket;
+console.log(`${ticket}`);
+console.log("i need popcorn dear");
+let popcorn=await getPopcorn;
+console.log(`${popcorn}`);
+console.log("i need butter dear");
+let butter=await getButter;
+console.log(`${butter}`);
+console.log("i need cold drinks dear");
+let coldDrinks=await getColdDinks;
+console.log(`${coldDrinks}`);
+console.log("lets go to movie dear");
+
+return ticket;
+
+}
+movie().then((t)=>console.log(t));
+console.log("person 4");
+console.log("person 5");
